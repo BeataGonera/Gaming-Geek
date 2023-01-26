99f7b34721bad14dd95bf3f7bd76e908f950354d
Beata Gonera <beataiwonagonera@gmail.com>
2023-01-26 23:09:43 +0100

    sass added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
+import { Signup } from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-
+      <Signup/>
+   
     </div>
   );
 }
---
+++ src/pages/signup/Signup.tsx
@@ -0,0 +1,11 @@
+import styles from './Signup.module.scss';
+
+export const Signup = () => {
+    return ( 
+        <div className={styles.h1}>
+            <p>Sign up</p>
+            <h3>Checking sass</h3>
+        </div>
+     );
+}
+ 
