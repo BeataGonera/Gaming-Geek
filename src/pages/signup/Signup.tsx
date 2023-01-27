a1f2ed002630283574e70ff66060760be3225529
Beata Gonera <beataiwonagonera@gmail.com>
2023-01-27 11:27:22 +0100

    signup page background

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -2,10 +2,13 @@ import styles from './Signup.module.scss';
 
 export const Signup = () => {
     return ( 
-        <div className={styles.h1}>
-            <p>Sign up</p>
-            <h3>Checking sass</h3>
+        <div className={styles.backgroundPicture}>
+         <div className={styles.backgroundColor}>
+            <div className={styles.signupFormContainer}>
+            </div>
         </div>
+        </div>
+       
      );
 }
  
