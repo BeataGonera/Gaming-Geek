58cefec23bcf8a11ffb214983e1f68cfb4e693d6
Beata Gonera <beataiwonagonera@gmail.com>
2023-03-01 11:36:06 +0100

    testing sending notifications

---
+++ public/firebase-messaging-sw.ts
@@ -0,0 +1,35 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-sw.js');
+
+import { initializeApp } from "firebase/app"
+import { getMessaging } from "firebase/messaging/sw";
+import { onBackgroundMessage } from "firebase/messaging/sw";
+
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseConfig = {
+    apiKey: "AIzaSyAI8mSZiAtG2gA4ws050IvZF2wuzDF623o",
+    authDomain: "geeks-grouper.firebaseapp.com",
+    databaseURL: "https://geeks-grouper-default-rtdb.firebaseio.com",
+    projectId: "geeks-grouper",
+    storageBucket: "geeks-grouper.appspot.com",
+    messagingSenderId: "955217459850",
+    appId: "1:955217459850:web:d70035e5765663f0a60b73"
+};
+
+initializeApp(firebaseConfig)
+const messaging = getMessaging();
+onBackgroundMessage(messaging, (payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
